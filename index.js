7b043f6a68d03f0f8358c8b185ab9ceae70e4625
Cristian <cri.jimenez24@gmail.com>
2024-05-25 20:52:13 -0400

    Se creo la ruta para agregar deportes y visualizar deportes a traves de rutas get

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import path from "path";
+import { nanoid } from "nanoid";
+import { writeFile, readFile } from "fs/promises";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 const __dirname = import.meta.dirname;
+
+const pathFile = __dirname + "/data/deportes.json";
 
 // public directory
 app.use(express.static(path.join(__dirname, "/public")));
@@ -33,6 +36,51 @@ app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/desafio", async (req, res) => {
+  res.render("desafio");
+});
+
+app.get("/deportes" , async (req, res) => {
+
+  try {
+    const stringDeportes = await readFile(pathFile, "utf-8");
+    const deportes = JSON.parse(stringDeportes);
+    
+    return res.json({deportes})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ok:false})
+  }
+
+})
+
+app.get("/agregar", async (req, res) => {
+  let mensaje = "";
+
+  try {
+    const { nombre, precio } = req.query;
+
+    const newDeporte = {
+      id: nanoid(10),
+      deporte: nombre,
+      precio,
+    };
+
+    const stringDeportes = await readFile(pathFile, "utf-8");
+    const deportes = JSON.parse(stringDeportes);
+
+    deportes.push(newDeporte);
+
+    await writeFile(pathFile, JSON.stringify(deportes));
+
+    return res.json({ ok: newDeporte });
+  } catch (error) {
+    mensaje = "No fue posible crear el registro";
+    console.log(error);
+    res.render("desafio", { mensaje, error });
+  }
 });
 
 // 404 para cualquier otra ruta
